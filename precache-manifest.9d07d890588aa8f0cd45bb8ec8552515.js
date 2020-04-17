69eb066db6c8c43cf1095f328e972dcde4e2a381
zura12337 <zura.kurta@gmail.com>
2020-04-17 12:58:10 +0400

    Updates

---
+++ precache-manifest.9d07d890588aa8f0cd45bb8ec8552515.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8637ade4f83d4c5d18380e65ec74a729",
+    "url": "/job-listing/index.html"
+  },
+  {
+    "revision": "fdcb9a68ce9e9d75346c",
+    "url": "/job-listing/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "ca5212a7910f8f5bdca7",
+    "url": "/job-listing/static/css/main.766413ed.chunk.css"
+  },
+  {
+    "revision": "fdcb9a68ce9e9d75346c",
+    "url": "/job-listing/static/js/2.0aa98922.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/job-listing/static/js/2.0aa98922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5212a7910f8f5bdca7",
+    "url": "/job-listing/static/js/main.8b494260.chunk.js"
+  },
+  {
+    "revision": "ee4c63a2fa2d7ea49fc6",
+    "url": "/job-listing/static/js/runtime-main.51e4bfc0.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listing/static/media/bg-header-desktop.6e2f7196.svg"
+  }
+]);
